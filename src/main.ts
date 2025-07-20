fdd7bc40f34c7db01988e469d88defc2a1437829
John Mutua Makumi <johnmutuamakumi1@gmail.com>
2025-07-20 07:39:20 +0300

    revert: hono server to be self-contained in githubWebHook.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
 import { Command } from 'commander';
-import { Hono } from 'hono';
 import { addSystemdCommands } from './commands/systemd';
 import { addUserCommands } from './commands/user';
 import { addOrchestrateCommand } from './commands/orchestrate';
-import { addGithubWebhookRoute } from './githubWebHook';
 
 const program = new Command();
 
@@ -16,21 +14,4 @@ addSystemdCommands(program);
 addUserCommands(program);
 addOrchestrateCommand(program);
 
-// If no arguments are provided, start the Hono server
-if (process.argv.length === 2) {
-  interface Env {
-    Variables: {
-      rawBody: string;
-    };
-  }
-  const app = new Hono<Env>();
-  addGithubWebhookRoute(app);
-
-  console.log('Hono server listening on port 8787');
-  Bun.serve({
-    port: 8787,
-    fetch: app.fetch,
-  });
-} else {
-  program.parse(process.argv);
-}
+program.parse(process.argv);
